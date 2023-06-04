35c0436581878b9b8a4097e4a486bd0a25762f1e
Ari Zeltser <ari.zeltser@gmail.com>
2023-06-04 04:11:57 -0700

    revert changes made for testing purposes

--- main.py
+++ main.py
@@ -26,34 +26,33 @@ if previous_month == 0:
 previous_month_name = datetime.date(1900, previous_month, 1).strftime('%B')
 year_name = str(year)
 
-if current_date.day == 4:
-    # get history
-    history = ytmusic.get_history()
+# get history
+history = ytmusic.get_history()
 
-    # list of ok results of played variable
-    ok_played = ['Today','Yesterday','Last week',previous_month_name+' '+year_name]
+# list of ok results of played variable
+ok_played = ['Yesterday','Last week',previous_month_name+' '+year_name]
 
-    times_played = {}
-    for item in history:
-        if item['played'] in ok_played:
-            if item['title'] in times_played:
-                times_played[item['title']] += 1
-            else:
-                times_played[item['title']] = 1
+times_played = {}
+for item in history:
+    if item['played'] in ok_played:
+        if item['title'] in times_played:
+            times_played[item['title']] += 1
+        else:
+            times_played[item['title']] = 1
 
-    # sort by most played
-    sorted_times_played = sorted(times_played.items(), key=lambda x: x[1], reverse=True)
+# sort by most played
+sorted_times_played = sorted(times_played.items(), key=lambda x: x[1], reverse=True)
 
-    # print results
-    message = ''
-    print('Top 10 songs played in ' + previous_month_name + ' ' + year_name)
-    for i in range(10):
-        if sorted_times_played[i][1] <= 1:
-            message = message + 'No more songs played more than once' + '\n'
-            print('No more songs played more than once')
-            break
-        message = message + str(i+1) + '. ' + sorted_times_played[i][0] + ' - ' + str(sorted_times_played[i][1]) + ' times' + '\n'
-        print(str(i+1) + '. ' + sorted_times_played[i][0] + ' - ' + str(sorted_times_played[i][1]) + ' times')
+# print results
+message = ''
+print('Top 10 songs played in ' + previous_month_name + ' ' + year_name)
+for i in range(10):
+    if sorted_times_played[i][1] <= 1:
+        message = message + 'No more songs played more than once' + '\n'
+        print('No more songs played more than once')
+        break
+    message = message + str(i+1) + '. ' + sorted_times_played[i][0] + ' - ' + str(sorted_times_played[i][1]) + ' times' + '\n'
+    print(str(i+1) + '. ' + sorted_times_played[i][0] + ' - ' + str(sorted_times_played[i][1]) + ' times')
 
 
 smtp_server = "smtp.gmail.com" 
